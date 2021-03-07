984f0274cb3e3457ed334fdd8a8272df7825d3dd
imed <imedzenned@yahoo.fr>
2021-03-07 22:29:34 +0100

    First commit

---
+++ src/main/java/tn/utm/isi/imd/MotcApplication.java
@@ -0,0 +1,13 @@
+package tn.utm.isi.imd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotcApplication.class, args);
+	}
+
+}
