2b9a460371ebbac3443495aabcf20ea230cfbfca
imed <imedzenned@yahoo.fr>
2021-03-08 01:09:09 +0100

    fix tomcat problem

--- src/main/java/tn/utm/isi/imd/config/JwtAuthenticationFilter.java
+++ src/main/java/tn/utm/isi/imd/config/JwtAuthenticationFilter.java
@@ -20,9 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-
-
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
